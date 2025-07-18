d160d09354e24b8901800b02f13fbb2ac69690cc
Professor Comic <professorcomic1@gmail.com>
2025-07-18 21:19:05 +0300

    All changes

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('sermons/', views.sermons, name='sermons'),
     path('events/', views.events, name='events'),
-    path('contacts/', views.contacts, name='contacts'),
+    path('contact/', views.contact, name='contact'),
+    path('give/', views.give, name='give'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,29 +1,19 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'mainapp/home.html')
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, 'mainapp/about.html')
 
 def sermons(request):
-<<<<<<< HEAD
     return render(request, 'mainapp/sermons.html')
-
-def give(request):
-    return render(request, 'mainapp/give.html')
 
 def events(request):
     return render(request, 'mainapp/events.html')
 
 def contact(request):
     return render(request, 'mainapp/contact.html')
-=======
-    return render(request, 'sermons.html')
 
-def events(request):
-    return render(request, 'events.html')
-
-def contacts(request):
-    return render(request, 'contacts.html')
->>>>>>> 0bf4b630fa1318caf786ac0e69a9ea06922ce338
+def give(request):
+    return render(request, 'mainapp/give.html')
