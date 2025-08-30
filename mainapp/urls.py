99d61bef2044a8ef54107c8bdab2e5d7d6669911
Professor Comic <professorcomic1@gmail.com>
2025-08-30 16:10:23 +0300

    push all changes

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -4,8 +4,12 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('leadership/', views.leadership, name='leadership'),
+    path('ministries/', views.ministries, name='ministries'),
     path('sermons/', views.sermons, name='sermons'),
+    path('blog/', views.blog, name='blog'),
     path('events/', views.events, name='events'),
     path('contact/', views.contact, name='contact'),
+    path('berean-times-magazine/', views.berean_times_magazine, name='berean_times_magazine'),
     path('give/', views.give, name='give'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -6,14 +6,26 @@ def home(request):
 def about(request):
     return render(request, 'mainapp/about.html')
 
+def leadership(request):
+    return render(request, 'mainapp/leadership.html')
+
+def ministries(request):
+    return render(request, 'mainapp/ministries.html')
+
 def sermons(request):
-    return render(request, 'mainapp/sermons.html')
+    return render(request, 'mainapp/sermons.html') 
+
+def blog(request):
+    return render(request, 'mainapp/blog.html')
 
 def events(request):
     return render(request, 'mainapp/events.html')
 
 def contact(request):
     return render(request, 'mainapp/contact.html')
+
+def berean_times_magazine(request):
+    return render(request, 'mainapp/berean_times_magazine.html')
 
 def give(request):
     return render(request, 'mainapp/give.html')
